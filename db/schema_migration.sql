cbdd6b3c6f2ab217de2b0d862af69aa0b14545eb
Sarah <sarahlchavez@gmail.com>
2021-04-29 05:17:36 -0700

    commit tree 3e6b654694f7ae4fe259fef2cb4f0c794a5d24dc

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,2 +1,8 @@
 -- Write your sql commands to create your db schema here.
+CREATE TABLE pokemon (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    type TEXT    
+);
+
 -- It should create a table in a new table in the db/ directory.
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,42 @@
+require "pry"
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    # def initialize(id, name, type, db)
+    #     @id = id
+    #     @name = name
+    #     @type = type
+    #     @db = db
+    # end
+
+    # Mass assignment
+    def initialize(attr_hash = {})
+        attr_hash.each do |key, value|
+            self.send("#{key}=", value) if respond_to?("#{key}=")
+        end
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type) 
+            VALUES (?, ?);
+        SQL
+
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+            SELECT * 
+            FROM pokemon
+            WHERE id = ?;
+        SQL
+
+        row = db.execute(sql, id)[0]
+        Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
+        #Pokemon.new(id: row[0][0], name: row[0][1], type: row[0][2], db: db)
+    end
 end
+
+
